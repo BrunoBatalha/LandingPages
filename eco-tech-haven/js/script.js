f861b55bd67ae7826b2292db99ebc9149e1a773b
Bruno Batalha <brunobatalha37@gmail.com>
2023-02-19 23:23:10 -0400

    echo-tech-haven/feat: add background in navbar when move to bottom, add anchor links

--- eco-tech-haven/js/script.js
+++ eco-tech-haven/js/script.js
@@ -1,7 +1,8 @@
 (() => {
-    function applyAnimationWhenElementsInViewport() {
+    function manageAnimations() {
         const elements = document.querySelectorAll("[data-anime]");
         const onScroll = () => {
+            console.log('tewste')
             let amountElementsAnimated = 0;
             elements.forEach(el => {
                 if (el.classList.contains("anime")) {
@@ -18,27 +19,37 @@
         }
 
         document.addEventListener("scroll", onScroll)
+
+        function isElementInViewport(el) {
+            var top = el.offsetTop;
+            var left = el.offsetLeft;
+            var width = el.offsetWidth;
+            var height = el.offsetHeight;
+
+            while (el.offsetParent) {
+                el = el.offsetParent;
+                top += el.offsetTop;
+                left += el.offsetLeft;
+            }
+
+            return (
+                top < (window.pageYOffset + window.innerHeight) &&
+                left < (window.pageXOffset + window.innerWidth) &&
+                (top + height) > window.pageYOffset &&
+                (left + width) > window.pageXOffset
+            );
+        }
     }
 
-    function isElementInViewport(el) {
-        var top = el.offsetTop;
-        var left = el.offsetLeft;
-        var width = el.offsetWidth;
-        var height = el.offsetHeight;
-
-        while (el.offsetParent) {
-            el = el.offsetParent;
-            top += el.offsetTop;
-            left += el.offsetLeft;
+    function manageNavbar() {
+        const headerEl = document.querySelector('.js-header');
+        const onScroll = () => {
+            headerEl.setAttribute('style', window.pageYOffset === 0 ? '' : 'background-color: #0d1c22a1')
         }
 
-        return (
-            top < (window.pageYOffset + window.innerHeight) &&
-            left < (window.pageXOffset + window.innerWidth) &&
-            (top + height) > window.pageYOffset &&
-            (left + width) > window.pageXOffset
-        );
+        document.addEventListener("scroll", onScroll)
     }
 
-    applyAnimationWhenElementsInViewport()
+    manageNavbar();
+    manageAnimations()
 })()
